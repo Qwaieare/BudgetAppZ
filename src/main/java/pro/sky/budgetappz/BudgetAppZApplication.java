6670ba8c1fda7401cefdfa95e7cfca610ad9b3d6
Qwaieare <gb-grand@mail.ru>
2023-04-02 15:35:04 +0300

    BudgetAppZ - практика от Зураба https://github.com/Qwaieare/BudgetAppZ.git

---
+++ src/main/java/pro/sky/budgetappz/BudgetAppZApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.budgetappz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetAppZApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BudgetAppZApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/budgetappz/BudgetAppZApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.budgetappz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetAppZApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
